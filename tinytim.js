2663c8ba75b8957f32e6d00df7417b2293b5af50
Mauricio Piacentini <mauricio@tabuleiro.com>
2014-09-26 10:21:18 -0300

    change function name, remove full version and module initialization

--- tinytim.js
+++ tinytim.js
@@ -1,13 +1,13 @@
 /*!
-* Tim (lite)
+* minitemplate 
+*   github.com/piacentini/minitemplate
+*
+*   Forked from:
 *   github.com/premasagar/tim
 *
 *//*
-    A tiny, secure JavaScript micro-templating script.
+    A tiny, secure JavaScript minitemplating script.
 *//*
-
-    by Premasagar Rose
-        dharmafly.com
 
     license
         opensource.org/licenses/mit-license.php
@@ -15,58 +15,42 @@
     **
 
     creates global object
-        tim
+        minitemplate
 
     **
-        
-    v0.3.0
-        
 */
 
-(function(name, definition, context) {
-    if (typeof module != 'undefined' && module.exports) {
-        module.exports = definition();
-    } else if (typeof context['define'] == 'function' && context['define']['amd']) {
-        define(definition);
-    } else {
-        context[name] = definition();
-    }
-})('tim', function() {
+var minitemplate = (function(){
+    "use strict";
 
-    var tim = (function(){
-        "use strict";
+    var start   = "{{",
+        end     = "}}",
+        path    = "[a-z0-9_$][\\.a-z0-9_]*", // e.g. config.person.name
+        pattern = new RegExp(start + "\\s*("+ path +")\\s*" + end, "gi"),
+        undef;
+    
+    return function(template, data){
+        // Merge data into the template string
+        return template.replace(pattern, function(tag, token){
+            var path = token.split("."),
+                len = path.length,
+                lookup = data,
+                i = 0;
 
-        var start   = "{{",
-            end     = "}}",
-            path    = "[a-z0-9_$][\\.a-z0-9_]*", // e.g. config.person.name
-            pattern = new RegExp(start + "\\s*("+ path +")\\s*" + end, "gi"),
-            undef;
-        
-        return function(template, data){
-            // Merge data into the template string
-            return template.replace(pattern, function(tag, token){
-                var path = token.split("."),
-                    len = path.length,
-                    lookup = data,
-                    i = 0;
-
-                for (; i < len; i++){
-                    lookup = lookup[path[i]];
-                    
-                    // Property not found
-                    if (lookup === undef){
-                        throw "tim: '" + path[i] + "' not found in " + tag;
-                    }
-                    
-                    // Return the required value
-                    if (i === len - 1){
-                        return lookup;
-                    }
+            for (; i < len; i++){
+                lookup = lookup[path[i]];
+                
+                // Property not found
+                if (lookup === undef){
+                    console.log "minitemplate: '" + path[i] + "' not found in " + tag;
+                    lookup = "";
                 }
-            });
-        };
-    }());
-
-    return tim;
-
-}, this);
+                
+                // Return the required value
+                if (i === len - 1){
+                    return lookup;
+                }
+            }
+        });
+    };
+}());
